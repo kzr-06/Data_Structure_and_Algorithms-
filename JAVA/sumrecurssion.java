59c27762b359eb3d5ec29d7a7ea47bd337087857
Kesar <kesar.skb@gmail.com>
2026-03-24 23:21:31 +0530

    new

---
+++ JAVA/sumrecurssion.java
@@ -0,0 +1,25 @@
+package JAVA;
+import java.util.Scanner;
+
+public class sumrecurssion {
+    // int Sum(int n){
+    //     if (n==0){
+    //         return 0;
+    //     }
+    //     return k;
+
+    // }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        // int n = input.nextInt();
+        // sumrecurssion obj = new sumrecurssion(); // create an object
+        // int k = obj.Sum(n); 
+        // System.out.println(k);
+        	int a = 5;  
+	double b = 0;  
+	System.out.println(a / b);
+
+        input.close();
+
+    }
+}
