90cb8c7bdc93615043266a4151ca30fe66021c06
Kesar <kesar.skb@gmail.com>
2026-03-28 23:33:17 +0530

    ..

---
+++ DSA/BubbleSort/Sort.java
@@ -0,0 +1,30 @@
+package DSA.BubbleSort;
+
+import java.util.*;
+
+public class Sort {
+        public static void main(String[] args) {
+        int[] arr ={15,16,6,8,5};
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<arr.length-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        // for(int i=0;i<arr.length-1;i++){
+        //     for(int j=0;j<arr.length-1-i;j++){
+        //         if(arr[j]>arr[j+1]){
+        //             int temp = arr[j];
+        //             arr[j] = arr[j+1];
+        //             arr[j+1]=temp;
+        //         }
+        //     }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
+
+
+// Time Complexity :- O(n^2)
